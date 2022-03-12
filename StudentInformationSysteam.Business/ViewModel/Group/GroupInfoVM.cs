12e3ac1cf8289f9b0253937e6415a9830c92ac7d
MzadeTural <tu201906140@code.edu.az>
2022-03-12 13:17:20 +0400

    Create GroupInfo view model

---
+++ StudentInformationSysteam.Business/ViewModel/Group/GroupInfoVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentInformationSysteam.Business.ViewModel.Group
+{
+    public class GroupInfoVM
+    {
+        public string Name { get; set; }
+        public string FacultyName { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
