6bbdb494a1383fd258158cc9ed430830c6639e58
MzadeTural <tu201906140@code.edu.az>
2022-03-13 19:39:18 +0400

    Create Paginate.cs

---
+++ StudentInformationSysteam.Business/ViewModel/Paginate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentInformationSysteam.Business.ViewModel
+{
+    public class Paginate<T>
+    {
+
+        public Paginate(List<T> item, int currentPAge, int pageCount)
+        {
+            Item = item;
+            CurrentPage = currentPAge;
+            PageCount = pageCount;
+        }
+        public List<T> Item { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+    }
+}
