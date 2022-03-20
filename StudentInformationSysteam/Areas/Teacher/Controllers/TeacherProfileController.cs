bb2724f6ba02f442a1f0afde847526845de390bc
MzadeTural <tu201906140@code.edu.az>
2022-03-20 19:11:46 +0400

    Add Teacher profile view

--- StudentInformationSysteam/Areas/Teacher/Controllers/TeacherProfileController.cs
+++ StudentInformationSysteam/Areas/Teacher/Controllers/TeacherProfileController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentIformationSysteam.Core.Models;
 using StudnetInformationSysteam.Data.DAL;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 
 namespace StudentInformationSysteam.Areas.Teacher.Controllers
 {
@@ -11,24 +11,26 @@ namespace StudentInformationSysteam.Areas.Teacher.Controllers
     public class TeacherProfileController : Controller
     {
         private readonly AppDbContext _context;
-        private readonly UserManeger<AppUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public TeacherProfileController(AppDbContext context,UserManeger<AppUser> userManager)
+        public TeacherProfileController(AppDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+
             _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
-            AppUser userI = await _userManager.GetUserAsync((System.Security.Claims.ClaimsPrincipal)User);
+            AppUser userI = await _userManager.GetUserAsync(User);
 
             AppUser userDetails = await _context.Users
-               .Include(ab => ab.UserGroups)             
-               .Include(u => u.Gender)
-               .FirstOrDefaultAsync(n => n.Id == userI.Id);
+                                                .Include(u => u.Gender)
+
+                                                .FirstOrDefaultAsync(n => n.Id == userI.Id);
 
 
             return View(userDetails);
+
         }
     }
 }
