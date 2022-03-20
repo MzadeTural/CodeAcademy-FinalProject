d0ac6f54768e092135541a4ebd892b3283df00f5
MzadeTural <tu201906140@code.edu.az>
2022-03-20 23:07:31 +0400

    Add features show notification

--- StudentInformationSysteam/Areas/Teacher/Controllers/TeacherHomeController.cs
+++ StudentInformationSysteam/Areas/Teacher/Controllers/TeacherHomeController.cs
@@ -6,8 +6,20 @@ namespace StudentInformationSysteam.Areas.Teacher.Controllers
     [Authorize(Roles = "Teacher")]
     public class TeacherHomeController : Controller
     {
+        private readonly object _context;
+
+        public TeacherHomeController(object context)
+        {
+            _context = context;
+        }
+
+        public TeacherHomeController()
+        {
+
+        }
         public IActionResult Index()
         {
+            ViewBag.Notifications = _context.Notifications.OrderByDescending(n => n.Id).Take(5).ToList();
             return View();
         }
     }
--- StudentInformationSysteam/Controllers/HomeController.cs
+++ StudentInformationSysteam/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StudentIformationSysteam.Core.Models;
+using StudnetInformationSysteam.Data.DAL;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudentInformationSysteam.Controllers
@@ -8,16 +10,19 @@ namespace StudentInformationSysteam.Controllers
     public class HomeController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _context;
 
-        public HomeController(UserManager<AppUser> userManager)
+        public HomeController(UserManager<AppUser> userManager,AppDbContext context)
 
         {
             _userManager = userManager;
+            _context = context;
         }
         public async Task<IActionResult> Index()
         {
             AppUser userI = await _userManager.GetUserAsync(User);
             ViewBag.UserName=userI.FullName;
+            ViewBag.Notifications = _context.Notifications.OrderByDescending(n=>n.Id).Take(5).ToList();
             return View();
         }
     }
